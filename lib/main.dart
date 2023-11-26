8beb9406e67ebbb8f57cb47db1c1389b660dbbc1
Your GitHub Username <Your GitHub Email>
2023-11-27 01:50:43 +0500

    updating some layout datae

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_apis_app/View/Homepage.dart';
+import 'package:movies_apis_app/responsivelayout.dart';
 import 'package:movies_apis_app/View/DetailedScreen.dart';
+import 'package:movies_apis_app/View/desktop/DeskHome.dart';
 import 'package:movies_apis_app/Get/FavouriteController.dart';
 
 void main() {
@@ -13,14 +15,17 @@ class MyApp extends StatelessWidget {
   final FavouriteController _favouriteController = Get.find();
 
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Obx(
       () => GetMaterialApp(
         title: 'Flutter Demo',
         theme: _favouriteController.isBright.value
             ? ThemeData.dark()
             : ThemeData.light(),
-        home: MyHomePage(),
+        home: ResponsiveLayout(
+          mobile: MyHomePage(),
+          desktop: DesktHome(),
+        ),
         debugShowCheckedModeBanner: false,
       ),
     );
